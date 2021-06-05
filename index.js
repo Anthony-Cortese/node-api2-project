01965b2393f829639476c3396cde8ad13891e488
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-06-04 18:58:26 -0600

    I got all my routers to work except for my Put and my Delete, will work on those

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require('./api/server.js');
+
+server.listen(4000, () => {
+    console.log('you got a sick server running on port 4000')
+});
